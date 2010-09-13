fab18895ccfa1d8fba6e4eb2c02cabcf75b3e4b4
derknob <derknob@8f2d0c2d-9ced-9820-663d-73db46f1663d>
2010-09-13 19:29:25 +0000

    added file header

--- trunk/backends/platform/sally/AppScummVM.cpp
+++ trunk/backends/platform/sally/AppScummVM.cpp
@@ -1,3 +1,32 @@
+////////////////////////////////////////////////////////////////////////////////////////////////////
+/// \file	sally\AppScummVM.cpp
+///
+/// \brief	Implements the application scummvm class. 
+///
+/// \author	Christian Knobloch
+/// \date	13.09.2010
+///
+/// This file is part of the Sally Project
+/// 
+/// Copyright(c) 2008-2010 Sally Project
+/// http://www.sally-project.de/
+///
+/// This program is free software; you can redistribute it and/or
+/// modify it under the terms of the GNU General Public License
+/// as published by the Free Software Foundation; either version 3
+/// of the License, or (at your option) any later version.
+///
+/// This program is distributed in the hope that it will be useful,
+/// but WITHOUT ANY WARRANTY; without even the implied warranty of
+/// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+/// GNU General Public License for more details.
+///
+/// You should have received a copy of the GPL along with this
+/// program. If not, go to http://www.gnu.org/licenses/gpl.html
+/// or write to the Free Software Foundation, Inc.,
+/// 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
 #include "AppScummVM.h"
 
 #define pixelSpace int
