3a49dd995b0d792481893c46490eaa831c6c7fa7
derknob <derrealknob@gmail.com>
2010-09-13 19:29:25 +0000

    added file header

--- backends/platform/sally/Define.h
+++ backends/platform/sally/Define.h
@@ -1,3 +1,32 @@
+////////////////////////////////////////////////////////////////////////////////////////////////////
+/// \file	sally\Define.h
+///
+/// \brief	Declares the defines. 
+///
+/// \author	Christian Knobloch
+/// \date	13.09.2010
+///
+/// This file is part of the Sally Project
+/// 
+/// Copyright(c) 2008-2010 Sally Project
+/// http://www.sally-project.de/
+///
+/// This program is free software; you can redistribute it and/or
+/// modify it under the terms of the GNU General Public License
+/// as published by the Free Software Foundation; either version 3
+/// of the License, or (at your option) any later version.
+///
+/// This program is distributed in the hope that it will be useful,
+/// but WITHOUT ANY WARRANTY; without even the implied warranty of
+/// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+/// GNU General Public License for more details.
+///
+/// You should have received a copy of the GPL along with this
+/// program. If not, go to http://www.gnu.org/licenses/gpl.html
+/// or write to the Free Software Foundation, Inc.,
+/// 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
 #define GUI_APP_SHOW_MENU		GUI_APP_PLUGIN_ID + 1
 #define GUI_APP_FULLSCREEN		GUI_APP_PLUGIN_ID + 2
 #define GUI_APP_MOUSE_LEFT		GUI_APP_PLUGIN_ID + 3
